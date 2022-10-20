aee7ec3fd43ccdb05422fd99d55f7a6b5ed72228
Manuel Benini <manuelbenini1905@gmail.com>
2022-10-20 20:42:16 +0200

    Aggiunta validazione per i pacchetti di viaggio

--- Models/PacchettoViaggio.cs
+++ Models/PacchettoViaggio.cs
@@ -1,12 +1,25 @@
-﻿namespace webapp_travel_agency.Models
+﻿using System.ComponentModel.DataAnnotations;
+using Validations;
+
+namespace webapp_travel_agency.Models
 {
     public class PacchettoViaggio
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Il campo titolo è obbligatorio")]
         public string Titolo { get; set; }
+
+        [Required(ErrorMessage = "Il campo località è obbligatorio")]
         public string Località { get; set; }
+
+        [Required(ErrorMessage = "Il campo immagine è obbligatorio")]
         public string Immagine { get; set; }
+
         public string? Descrizione { get; set; }
-        public decimal Prezzo { get; set; }
+
+        [MoreThanZeroValidation]
+        [Required(ErrorMessage = "il campo prezzo è obbligatorio")]
+        public decimal? Prezzo { get; set; }
     }
 }
---
+++ Models/Validation/MoreThanZeroValidationAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Validations
+{
+    public class MoreThanZeroValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                if ((decimal)value <= 0)
+                {
+                    return new ValidationResult("Il prezzo deve essere maggiore di 0");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
